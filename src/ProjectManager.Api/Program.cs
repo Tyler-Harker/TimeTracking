43294b2b250af82f697f0eb23cbb8c503af32161
Tyler Harker <me@tylerharker.com>
2026-05-05 12:30:33 -0700

    chore: updated

--- src/ProjectManager.Api/Program.cs
+++ src/ProjectManager.Api/Program.cs
@@ -145,7 +145,6 @@ if (app.Environment.IsDevelopment())
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.EnsureCreated();
     dbContext.Database.Migrate();
 }
 
--- src/ProjectManager.ServiceDefaults/obj/Debug/net10.0/ProjectManager.ServiceDefaults.AssemblyInfo.cs
+++ src/ProjectManager.ServiceDefaults/obj/Debug/net10.0/ProjectManager.ServiceDefaults.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManager.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73bb82a55285afe04e6a7c7ed7fc12cb3388f892")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cebe199d5ce34dc1430038094e094f2824d496")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManager.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManager.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
