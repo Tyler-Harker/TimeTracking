f78da6bc859fd56ff88e072a430e1a4bb14480da
Tyler Harker <me@tylerharker.com>
2026-05-10 21:00:41 -0700

    chore: updated

--- src/ProjectManager.Api/obj/Debug/net10.0/ProjectManager.Api.AssemblyInfo.cs
+++ src/ProjectManager.Api/obj/Debug/net10.0/ProjectManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffece7fbb9d48a148bad3fb29042c1f1b7a883e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d7a2bdbaa57cc3295b75cd0b222590f2a307fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
